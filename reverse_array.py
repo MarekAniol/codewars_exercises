b24de7a0a2f26b4695af33e495527987edcd4a00
MarekAniol <marekaniol1@gmail.com>
2022-03-28 09:56:01 +0200

    add files

--- reverse_array.py
+++
@@ -1,6 +0,0 @@
-def digitize(n):
-
-    return list(map(int, str(n)[::-1]))
-
-wynik = digitize(123456789)
-print(type(wynik[0]))
--- wesola_liczba.py
+++
@@ -1,9 +0,0 @@
-def funy_number(x: int) -> int:
-    print(len(str(x)))
-    y = [int(el) for el in list(str(x))]
-    x = sum(y)
-    print(x)
-    return funy_number(x) if x >= 10 else 1
-
-
-print(funy_number(36))
