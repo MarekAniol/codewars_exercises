c38394ed2fc5901a3e5868887c32bb1362c87b85
MarekAniol <marekaniol1@gmail.com>
2022-04-01 23:02:59 +0200

    add second function to alphabetical_grid

--- alphabetical_grid.py
+++ alphabetical_grid.py
@@ -1,7 +1,20 @@
-from string import ascii_lowercase
-def grid(n):
-    lc = list(ascii_lowercase)
-    for _ in lc:
+from string import ascii_lowercase as lca
+
+
+def grid_generator(n):
+    lc = list(lca)
+    for _ in range(n):
         line = " ".join(lc[0:n])
         lc.append(lc.pop(0))
         yield line
+
+
+def grid(n):
+    char = list((n // len(lca) + 1) * lca)
+    row = 0
+    r = ''
+    while n > row:
+        r += " ".join(char[0:n]) + "\n"
+        char.append(char.pop(0))
+        row += 1
+    return None if n < 0 else r[0:-1]
