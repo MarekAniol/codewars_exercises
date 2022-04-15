f6d69a22ddc58f12e9f43c4aec3f36828ab77cfe
MarekAniol <marekaniol1@gmail.com>
2022-04-15 21:44:04 +0200

    update

--- pig_it.py
+++ pig_it.py
@@ -1,2 +1,9 @@
+from re import sub
+
+
 def pig_it(text):
     return " ".join(el[1:] + el[0] + "ay" if not set('?!').intersection(el) else el for el in text.split())
+
+
+def pig_it_2(text):
+    return sub(r'(\w)(\w+)( ?)', r'\2\1ay\3', text)
